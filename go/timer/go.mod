9e4db68ff80e412000458b92f76f71f421c985f0
thousanda <k0k1senda@gmail.com>
2023-06-15 19:32:23 +0900

    GoのTimerを実装

---
+++ go/timer/go.mod
@@ -0,0 +1,3 @@
+module github.com/thousanda/sort/go/timer
+
+go 1.20
---
+++ go/timer/timer.go
@@ -0,0 +1,24 @@
+package timer
+
+import (
+	"fmt"
+	"time"
+)
+
+type Timer struct {
+	start time.Time
+	stop  time.Time
+}
+
+func (t *Timer) Start() {
+	t.start = time.Now()
+}
+
+func (t *Timer) Stop() {
+	t.stop = time.Now()
+}
+
+func (t *Timer) Print() {
+	elapsed := t.stop.Sub(t.start)
+	fmt.Println("Execution time: ", elapsed)
+}
